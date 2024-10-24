16fc8d2b367b6658eb61bfd21244112121abf5e4
hadeer <hadeeressam21@gmail.com>
2024-10-24 11:41:22 +0300

    apply MVVM clean architecture in React Native (simple)

---
+++ model/UserModle.js
@@ -0,0 +1,13 @@
+class UserModel {
+    constructor(
+        id,
+        name,
+        email
+    ) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+}
+
+export default UserModel;
